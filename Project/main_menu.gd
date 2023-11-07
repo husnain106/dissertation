b75602d4a821930478294f51a19fb69ab24232b0
Husnain Soharwardi <husnain.soharwardi@student.manchester.ac.uk>
2023-11-07 11:24:28 +0000

    All buttons point to a black screen with the message (blank screen)

--- Project/main_menu.gd
+++ Project/main_menu.gd
@@ -3,28 +3,28 @@ extends Control
 
 
 func _on_play_levels_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/black_screen.tscn")
 
 
 func _on_play_multiplayer_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/black_screen.tscn")
 
 
 func _on_free_space_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/black_screen.tscn")
 
 
 func _on_learn_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/black_screen.tscn")
 
 
 func _on_shop_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/black_screen.tscn")
 
 
 func _on_settings_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/black_screen.tscn")
 
 
 func _on_login_signup_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/black_screen.tscn")
