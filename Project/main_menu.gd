0747bab88db0821d3a7397a78d33b2a6dd8f019c
Husnain Soharwardi <husnain.soharwardi@student.manchester.ac.uk>
2023-11-07 11:18:51 +0000

    Simple main menu created with buttons that do not do anything

---
+++ Project/main_menu.gd
@@ -0,0 +1,30 @@
+extends Control
+
+
+
+func _on_play_levels_pressed():
+	pass # Replace with function body.
+
+
+func _on_play_multiplayer_pressed():
+	pass # Replace with function body.
+
+
+func _on_free_space_pressed():
+	pass # Replace with function body.
+
+
+func _on_learn_pressed():
+	pass # Replace with function body.
+
+
+func _on_shop_pressed():
+	pass # Replace with function body.
+
+
+func _on_settings_pressed():
+	pass # Replace with function body.
+
+
+func _on_login_signup_pressed():
+	pass # Replace with function body.
